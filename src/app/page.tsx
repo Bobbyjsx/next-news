b5c6cfcba22a59d8cc7d6cc544c7541c0bf6b0aa
bobbyjsx <ezealagodswill@gmail.com>
2023-04-16 11:01:49 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="flex items-center h-screen-my-auto">
+        HomePage
+      </h1>
+    </div>
+  );
+}
+
+export default page
